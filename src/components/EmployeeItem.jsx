93246a287c7496baa5533414daf0ae49f492e034
SamuelJJk <samueljean.j24@gmail.com>
2024-05-02 09:51:47 -0400

    first commit

---
+++ src/components/EmployeeItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function EmployeeItem(name,position,image) {
+  return (
+    <div className='emp_item'>
+      <div className="profile">
+        <img src="" alt="" />
+      </div>
+      <div className="infoBox">
+        <h1></h1>
+        <h2></h2>
+      </div>
+    </div>
+  )
+}
+
+export default EmployeeItem
